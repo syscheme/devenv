c10bff213c55347328dc994b09a8ffc1f3c82c91
Andy Shao <hui.shao@xor-media.tv>
2019-06-18 23:25:14 -0400

    exclude list

--- docker-build/zqBuildBox/centos7/collectSDKs.sh
+++ docker-build/zqBuildBox/centos7/collectSDKs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
+CWD=`pwd`
 for i in 3rdPartyKits SeaChangeKits ; do
         cd /opt/sdk/$i
-        tar cfvj /tmp/$i.tar.bz2 `ls -l |grep '\->'|grep -v 'boost\|net-snmp\|unixODBC' |sed -e 's/.* \([^ ]*\) \-> \([^ ]*\)$/ \1  \2/g'`
+        tar cfvj /tmp/$i.tar.bz2 -X ${CWD}/sdk-excl.txt `ls -l |grep '\->'|grep -v 'boost\|net-snmp\|unixODBC' |sed -e 's/.* \([^ ]*\) \-> \([^ ]*\)$/ \1  \2/g'`
 done;
 
